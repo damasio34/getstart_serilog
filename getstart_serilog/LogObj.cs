dbc42009fdc42039a1ead5c56b82494e00afb240
Darlan Damasio <damasio34@hotmail.com>
2018-04-14 23:39:03 -0300

    Commit inicial

---
+++ getstart_serilog/LogObj.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace getstart_serilog
+{
+    public class LogObj
+    {
+        public LogObj(string local, string mensagem)
+        {
+            this.Local = local;
+            this.Mensagem = mensagem;
+            this.DataEHora = DateTimeOffset.Now;
+        }
+
+        public DateTimeOffset DataEHora { get; set; }
+        public string Local { get; set; }
+        public string Mensagem { get; set; }
+    }
+}
---
+++ getstart_serilog/Program.cs
@@ -0,0 +1,27 @@
+﻿using Serilog;
+using Serilog.Events;
+using Serilog.Formatting.Json;
+using System;
+
+namespace getstart_serilog
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var log = new LoggerConfiguration()
+            .WriteTo.File(new JsonFormatter(), "../../Log/log_file.json", LogEventLevel.Information)
+            .WriteTo.Console()
+            .CreateLogger())
+            {
+                var logObj = new LogObj("Program", "Ops ocorreu um erro");
+
+                log.Information("Hello, Serilog!");
+                log.Warning("Goodbye, Serilog.");
+                log.Error<LogObj>("{@logObj}", logObj);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
